c54cc337ed94c9d21eaf9427fdfbb576183afb81
AnkithChowdary <ankithc29@gmail.com>
2023-10-23 00:06:27 +0530

    first commit

---
+++ Programs/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+
+
+        public static void main(String[] args)
+        {
+            boolean flag=true;
+            if(flag)
+                System.out.println("It is false");
+            else
+                System.out.println("It is true");
+        }
+
+}
