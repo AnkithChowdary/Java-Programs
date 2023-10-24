993a39be76b006010ec8f6e1c99d6f0c54d5c01e
AnkithChowdary <ankithc29@gmail.com>
2023-10-24 11:55:20 +0530

    Update

--- Programs/src/StringToInteger.java
+++ Programs/src/StringToInteger.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class StringToInteger {
+import java.util.Scanner;
+public class StringToInteger {
+   static Scanner sc=new Scanner(System.in);
+    public static void main(String[] args) {
+
+        String number=sc.next();
+        int ank=Integer.parseInt(number);
+        int d=12;
+        System.out.println(ank+d);
+        //Integer to string
+        int x=12;
+        String a=Integer.toString(x);
+        System.out.println(a);
+
+    }
 }
