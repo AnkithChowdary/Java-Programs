4ff13f62a44dcdd4f33e5d1f4e90369f41d72a2a
AnkithChowdary <ankithc29@gmail.com>
2023-10-29 13:25:27 +0530

    Update

--- Programs/src/DifferentInputWays.java
+++ Programs/src/DifferentInputWays.java
@@ -1,17 +1,17 @@
-import java.io.Console;
 import java.util.Scanner;
 public class DifferentInputWays {
     public static void main(String[] args){
 
-//        getInputFromConsole(year);
-        getInputFromScanner();
+
+       getInputFromScanner();
+        getInputFromConsole();
     }
     public static void getInputFromConsole(){
         String name= System.console().readLine("what's your name:");
         System.out.println("Your name is "+name);
     }
     public static void getInputFromScanner(){
-       Scanner an=new Scanner(System.in);
+       Scanner an =new Scanner(System.in);
        String k= an.next();
        System.out.println("your name is : "+k);
 
---
+++ Programs/src/arraylen.java
@@ -0,0 +1,16 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class arraylen
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		int[] an={10,20,30,40,50,60};
+		System.out.print(an.length);
+	}
+}
