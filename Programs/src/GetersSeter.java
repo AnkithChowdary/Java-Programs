d2de8d9903babe9ccbb957dba4b5dc0bdbd20077
AnkithChowdary <ankithc29@gmail.com>
2023-10-30 22:38:33 +0530

    Update

---
+++ Programs/src/GetersSeter.java
@@ -0,0 +1,21 @@
+// class student{
+//
+//    int id;
+//    String name;
+//
+//    }
+//
+//public class GetersSeter {
+//
+//
+//    public static void main(String[] args) {
+//
+//     student s1=new student();
+//
+//     s1.name="ankkth";
+//     s1.id=101;
+//
+//        System.out.println(s1.id);
+//
+//    }
+//}
