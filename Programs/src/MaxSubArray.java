20a403fbe4ed61db66d92e2436322a002ef91918
Ankith Chowdary <ankithc29@gmail.com>
2023-11-19 17:02:56 +0530

    Update

---
+++ Programs/src/MaxSubArray.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class MaxSubArray {
+  public static void sub(int arr[]){
+    int curr=0,max=Integer.MIN_VALUE;
+    for(int i=0; i<arr.length; i++){
+      
+      for(int j=i; j<arr.length; j++){
+        curr=0;
+          for(int k=i; k<=j; k++){
+          curr+=arr[k];
+          }
+          System.out.println(curr);
+          if(max<curr)
+          max=curr;
+        
+      }
+      
+    }
+    System.out.println("Total maxsum:"+max);
+  }
+  public static void main(String args[])
+  {
+     int arr[]={2,4,6,8,10};
+     sub(arr);
+     System.out.println(arr);
+  }
+}
