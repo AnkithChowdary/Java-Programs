dd4123fc90778e34a346c086d92d13dafed62bde
Ankith Chowdary <ankithc29@gmail.com>
2023-12-05 11:33:25 +0530

    Update

--- Programs/src/arrayListAdd.java
+++ Programs/src/arrayListAdd.java
@@ -6,6 +6,23 @@ public class arrayListAdd {
   list.add(1);
   list.add(2);
   System.out.println(list);
+  int element =list.get(0);
+  System.out.println(element);
+  //Remove operation
+  list.remove(0);
+  System.out.println(list);
+  // Set Operation
+  System.out.print("Set Operation: ");
+  list.set(0,10);
+  System.out.println(list);
+  list.add(5);
+  list.add(4);
+  System.out.println(list);
+  //Contains operation
+  System.out.println(list.contains(4));
+  System.out.println(list.contains(0));
+  list.add(1,7);
+  System.out.println(list);
   }
   
 }
