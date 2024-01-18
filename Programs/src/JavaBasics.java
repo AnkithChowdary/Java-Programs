42b2d64ad67cebd7ededb0b1fc214facce32138d
Ankith Chowdary <ankithc29@gmail.com>
2024-01-18 18:29:50 +0530

    Update

---
+++ Programs/src/JavaBasics.java
@@ -0,0 +1,8 @@
+public class JavaBasics {
+  public static void main(String[] args) {
+    float k=3.4f; //Explicitly mention because by default it is double
+    long s=41l;//Same for the long by defualt it is int
+    int g=1_00_00_000;          //In java we can use underscore to differentiate semicoln
+    System.out.println(g);
+  }
+}
