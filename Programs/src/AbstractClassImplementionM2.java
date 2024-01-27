e621ca31ef8f7f1fe2a507ce2048d12a69b8fe62
AnkithChowdary <ankithc29@gmail.com.>
2024-01-27 20:17:59 +0530

    Update

--- Programs/src/AbstractClassImplementionM2.java
+++ Programs/src/AbstractClassImplementionM2.java
@@ -1,12 +1,13 @@
+//Abstract class implemention using anonymous class 
+abstract class Car{
+  public void music(){
+    System.out.println("");
+  }
+  public abstract void fly();
+}
 public class AbstractClassImplementionM2{
 
-//Abstract class implemention using anonymous class 
-abstract static class Car{
-    public void music(){
-      System.out.println("");
-    }
-    public abstract void fly();
-}
+
   //Instead of using inheritance we can use anonymous way
 
 
