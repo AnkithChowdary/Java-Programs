ef0b0f8eecf35eecc86ddcd31f851a9daa0949c7
AnkithChowdary <ankithc29@gmail.com.>
2024-01-28 00:09:20 +0530

    Update

--- Programs/src/Abstract/AbstractClassImplementionM2.java
+++ Programs/src/Abstract/AbstractClassImplementionM2.java
@@ -10,8 +10,6 @@ public class AbstractClassImplementionM2{
 
 
   //Instead of using inheritance we can use anonymous way
-
-
   public static void main(String[] args){
      //Here we don't actually create the object and we can't create object for abstract classes 
      //We are simply writing the implemention of methods of that class
