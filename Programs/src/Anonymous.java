fab654c4420f880f2bc8223f54583c16f3e008f4
AnkithChowdary <ankithc29@gmail.com.>
2024-01-27 19:50:29 +0530

    Update

--- Programs/src/Anonymous.java
+++ Programs/src/Anonymous.java
@@ -8,9 +8,12 @@ class Unknown{
 }
 public class Anonymous{
 
-  public static void main(String[] args){
-     Unknown obj=new Unknown(){
-         public void display(){                       
+  public static void main(String[] args)
+  {
+     Unknown obj=new Unknown()
+     {
+         public void display()
+         {                       
           System.out.println("In Anonymous class");    //This method output will be printed instead of Unknown class method
          }
      };
