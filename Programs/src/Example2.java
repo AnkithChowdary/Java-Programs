4a074f1dff13b65018229542bf2cdefb1509ef87
Ankith <ankithc29@gmail.com>
2024-01-29 19:01:16 +0530

    Update

---
+++ Programs/src/Example2.java
@@ -0,0 +1,9 @@
+public class Example2 {
+    static int n,i,f=1;
+    public static void main(String[] args) {
+        n=5;
+        for(int i=1; i<=n; i++)
+            f*=i;
+        System.out.println(f);
+    }
+}
