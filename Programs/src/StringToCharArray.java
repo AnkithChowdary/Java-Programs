d6736a02aa90dce6fd5c02ed60212455086e3e5f
Ankith <ankithc29@gmail.com>
2024-02-10 16:34:25 +0530

    Update

---
+++ Programs/src/StringToCharArray.java
@@ -0,0 +1,10 @@
+public class StringToCharArray {
+  public static void main(String[] args) {
+    String a="ankith";
+    int k=a.length();
+    char [] arr=new char[k];
+    arr=a.toCharArray();
+    System.out.println(arr);
+
+  }
+}
